7f5aec6ad77c21fcab446b61394193eb96a2b01f
suryD <[surya.dy@outlook.fr] (mailto:MY_NAME@example.com)>
2023-04-05 11:44:06 +0200

    route

--- assets/app.js
+++ assets/app.js
@@ -5,10 +5,28 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
 
-console.log('Hello Webpack Encore !')
+// app.js
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
+// assets/app.js
+
+// returns the final, public path to this file
+// path is relative to this file - e.g. assets/images/logo.png
